8a63730fc7265541caa1416e4f2646e7c8da1995
davidemariadolfini-spec <davidemariadolfini@gmail.com>
2026-03-02 16:19:22 +0100

    Delete schema.sql

--- schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE greenhouse_stats (
-  id int8 PRIMARY KEY GENERATED ALWAYS AS IDENTITY, -- BigInt per gestire milioni di log
-  created_at timestamptz DEFAULT now(),             -- Timestamp automatico con fuso orario
-  temp float8,                                      -- Precisione decimale per la temperatura
-  humidity float8,                                  -- Umidità relativa
-  soil_moisture float8                              -- Livello di idratazione del terreno
-);
